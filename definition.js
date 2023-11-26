59017d542f224f6b21b925d84b510645800d4be5
Tanvir Islam <freelancer.tanvir.bd@gmail.com>
2023-11-26 22:20:05 +0600

    Add files via upload

--- definition.js
+++ definition.js
@@ -1,12 +1,6 @@
-jQuery(document).ready(function($) {
-    // Variable to store the direction of scrolling
-    var isScrollingUp = false;
-
-    // Variable to store the width of the previews
-    var totalPreviewWidth = 0;
-
+jQuery(document).ready(function ($) {
     // Add a listener for text selection
-    $(document).on('mouseup', function() {
+    $(document).on('mouseup', function () {
         var selectedText = getSelectedText();
         if (selectedText !== '') {
             showPreview(selectedText);
@@ -28,10 +22,14 @@ jQuery(document).ready(function($) {
 
     // Function to display the preview
     function showPreview(text) {
+        // Create a unique ID for the preview
+        var previewId = 'text-preview-' + Date.now();
+
         // Create a preview div
         var previewDiv = $('<div>', {
-            class: 'wordMeaningContainer',
-            style: 'display: block; position: fixed; background: rgb(247, 247, 196); color: #333; font-family: Arial, sans-serif; max-width: 300px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); border-radius: 5px; overflow: hidden; transition: top 0.3s ease-in-out;'
+            id: previewId,
+            class: 'text-preview',
+            style: 'border-radius: 8px;',
         });
 
         // Get the selected range
@@ -39,99 +37,91 @@ jQuery(document).ready(function($) {
         var range = selection.getRangeAt(0);
         var boundingRect = range.getBoundingClientRect();
 
-        // Position the preview initially at the selected word's bottom
-        var topPosition = boundingRect.bottom + window.scrollY;
-        var leftPosition = boundingRect.left + window.scrollX + totalPreviewWidth;
-
-        // Check if the preview fits in the viewport vertically
-        if (topPosition + previewDiv.height() > window.innerHeight) {
-            // If it doesn't fit below, move it to the top
-            topPosition = boundingRect.top + window.scrollY - previewDiv.height();
-        }
-
-        // Check if the preview fits in the viewport horizontally
-        if (leftPosition + previewDiv.width() > window.innerWidth) {
-            // If it doesn't fit on the right, adjust its left position
-            leftPosition = window.innerWidth - previewDiv.width();
-        }
-
-        // Set the calculated positions
+        // Position the preview below the selected text
         previewDiv.css({
-            top: topPosition + 'px',
-            left: leftPosition + 'px'
+            position: 'absolute',
+            top: boundingRect.bottom + window.scrollY + 'px',
+            left: boundingRect.left + window.scrollX + 'px',
+            backgroundColor: '#fff',
+            padding: '10px',
+            border: '1px solid #ccc',
+            borderRadius: '8px',
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+            zIndex: 9999,
+            transition: 'top 0.3s ease-in-out',
+        });
+
+        // Close button
+        var closeButton = $('<span>', {
+            class: 'close-btn',
+            html: '&times;',
+            style: 'position: absolute; top: 5px; right: 5px; cursor: pointer; font-size: 16px;',
+            click: function () {
+                hidePreview();
+            },
+        });
+
+        // Audio element
+        var audioElement = $('<audio>', {
+            controls: true,
+            style: 'width: 100%; margin-bottom: 10px;',
         });
 
         // Make AJAX request to the API
         $.ajax({
             url: 'https://api.dictionaryapi.dev/api/v2/entries/en/' + text,
             method: 'GET',
-            success: function(data) {
+            success: function (data) {
                 // Check if the API returned valid data
                 if (Array.isArray(data) && data.length > 0) {
-                    var word = data[0].word;
-                    var meaning = data[0].meanings[0].definitions[0].definition;
-                    var audioUrl = data[0].phonetics[0]?.audio || '';
-
-                    // Add word and meaning to the preview div
-                    var wordMeaningDiv = $('<div>', {
-                        class: 'wordMeaning',
-                        style: 'padding: 15px; text-align: left;'
+                    // Add word and definitions to the preview div
+                    previewDiv.append('<h3>' + data[0].word + '</h3>');
+                    data[0].meanings.forEach(function (meaning) {
+                        previewDiv.append(
+                            '<p><strong>' +
+                                meaning.partOfSpeech +
+                                ':</strong> ' +
+                                meaning.definitions[0].definition +
+                                '</p>'
+                        );
                     });
 
-                    wordMeaningDiv.append('<h2 style="margin-bottom: 10px;">' + word + '</h2>');
-                    wordMeaningDiv.append('<p style="font-size: 16px; line-height: 1.4; margin-bottom: 15px;">' + meaning + '</p>');
-
-                    // Add audio icon
+                    // Add audio source
+                    var audioUrl = data[0].phonetics[0]?.audio || '';
                     if (audioUrl !== '') {
-                        wordMeaningDiv.append('<audio controls style="width: 100%; margin-bottom: 10px;"><source src="' + audioUrl + '" type="audio/mpeg">Your browser does not support the audio element.</audio>');
+                        audioElement.append('<source src="' + audioUrl + '" type="audio/mpeg">Your browser does not support the audio element.</source>');
+                        previewDiv.append(audioElement);
                     }
 
-                    // Add close button
-                    wordMeaningDiv.append('<span style="position: absolute; top: 10px; right: 10px; cursor: pointer; font-size: 20px;" onclick="hidePreview()">✕</span>');
+                    // Append the close button to the preview div
+                    previewDiv.append(closeButton);
 
-                    previewDiv.append(wordMeaningDiv);
+                    // Append the preview div to the body
+                    $('body').append(previewDiv);
 
-                    // Add click event for words in the preview
-                    wordMeaningDiv.find('h2, p').on('click', function() {
-                        var clickedWord = $(this).text();
+                    // Add a click event for words in the preview to show a new preview
+                    previewDiv.on('click', 'p', function () {
+                        var clickedWord = $(this).text().split(':')[1].trim();
                         showPreview(clickedWord);
                     });
 
-                    // Update the total width of previews
-                    totalPreviewWidth += previewDiv.width();
+                    // Position the next preview beside the current one
+                    var nextPreviewLeft =
+                        previewDiv.outerWidth() + boundingRect.left + window.scrollX + 10; // 10px spacing
+                    previewDiv.next('.text-preview').css('left', nextPreviewLeft + 'px');
                 } else {
-                    previewDiv.append('<p style="padding: 15px;">No definition found for ' + text + '</p>');
+                    previewDiv.append('<p>No definition found for ' + text + '</p>');
                 }
             },
+            error: function () {
+                previewDiv.append('<p>Error fetching definition for ' + text + '</p>');
+            },
         });
-
-        // Append the preview div to the body
-        $('body').append(previewDiv);
-
-        // Add scrolling behavior
-        window.onscroll = function () {
-            var vertical_position = 0;
-            if (pageYOffset) // usual
-                vertical_position = pageYOffset;
-            else if (document.documentElement.clientHeight) // IE
-                vertical_position = document.documentElement.scrollTop;
-            else if (document.body) // IE quirks
-                vertical_position = document.body.scrollTop;
-
-            // Check the scrolling direction
-            isScrollingUp = vertical_position < topPosition;
-
-            // Adjust the top position of the preview div
-            previewDiv.css('top', (topPosition + (isScrollingUp ? -vertical_position : vertical_position)) + 'px');
-        };
     }
 
     // Function to hide the preview
-    window.hidePreview = function() {
-        $('.wordMeaningContainer').remove();
-        // Reset the total width of previews
-        totalPreviewWidth = 0;
-        // Remove the scrolling behavior when the preview is hidden
-        window.onscroll = null;
+    function hidePreview() {
+        // Remove all text previews
+        $('.text-preview').remove();
     }
 });
