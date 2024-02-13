b8d8247de1bf7204261531bf2be03101ab795570
Tanvir Islam <freelancer.tanvir.bd@gmail.com>
2024-02-13 19:50:55 +0600

    Update definition.php

--- definition.php
+++ definition.php
@@ -9,7 +9,20 @@ Description: This is a definition plugin to use in blog to show definition of an
 Author: Tanvir
 Version: 2.0
 Author URI: https://fiverwebsitedesign.com
-*/
+ *
+ * Selected Text Definition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Selected Text Definition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Selected Text Definition. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 // Enqueue the JavaScript file
 function text_preview_enqueue_scripts() {
